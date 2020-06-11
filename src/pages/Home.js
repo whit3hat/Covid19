c29c0bf7f33ef5b49a6f8e6fd1a0bd0612b271ad
Spencer Wood <spencer.wood@me.com>
2020-06-10 20:50:32 -0500

    added the container to the home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import StateStats from '../components/Card';
 import Nav from '../components/Nav';
+import {Container, Row, Col } from 'reactstrap';
 
 function Home() {
     return (
         <div>
             <Nav />
+            <Container className='themed-container'>
             <StateStats />
+           </Container>
         </div>
     );
 
